7d2b832332c1b43103882e6973cf4256437e1add
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2019-11-25 17:39:18 -0600

    Create test for gravity calculation

--- Space Turret/test/test.cpp
+++ Space Turret/test/test.cpp
@@ -1,3 +1,17 @@
 #include "catch.hpp"
 
-TEST_CASE("Basic math") { REQUIRE(1 == 1); }
+#include "ofMain.h"
+#include "calculations.h"
+
+TEST_CASE("Gravity calculations work") { 
+	ofVec2f player_position(0, 0);
+    ofVec2f planet_position(3, 4);
+    double multiplier = 250;
+
+	ofVec2f calculated_gravity =
+        calc::gravity(multiplier, player_position, planet_position);
+
+	ofVec2f expected_gravity(6, 8);
+
+	REQUIRE(calculated_gravity == expected_gravity);
+}
