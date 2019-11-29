18b298fa126a30e787feef90e32791c09ffd84a1
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2019-11-28 19:46:17 -0600

    Add extra test for gravity calculations

--- Space Turret/test/test.cpp
+++ Space Turret/test/test.cpp
@@ -3,15 +3,32 @@
 #include "ofMain.h"
 #include "calculations.h"
 
-TEST_CASE("Gravity calculations work") { 
-	ofVec2f player_position(0, 0);
-    ofVec2f planet_position(3, 4);
-    double multiplier = 250;
+TEST_CASE("Gravity calculations work") {
+    SECTION("Vector <3,4> works") {
+        ofVec2f player_position(0, 0);
+        ofVec2f planet_position(3, 4);
+        double multiplier = 250;
 
-	ofVec2f calculated_gravity =
-        calc::gravity(multiplier, player_position, planet_position);
+        ofVec2f calculated_gravity =
+            calc::gravity(multiplier, player_position, planet_position);
 
-	ofVec2f expected_gravity(6, 8);
+        ofVec2f expected_gravity(6, 8);
 
-	REQUIRE(calculated_gravity == expected_gravity);
+        REQUIRE(calculated_gravity.x == Approx(expected_gravity.x));
+        REQUIRE(calculated_gravity.y == Approx(expected_gravity.y));
+    }
+
+    SECTION("Vector <7,24> works") {
+        ofVec2f player_position(0, 0);
+        ofVec2f planet_position(7, 24);
+        double multiplier = 62500;
+
+        ofVec2f calculated_gravity =
+            calc::gravity(multiplier, player_position, planet_position);
+
+        ofVec2f expected_gravity(28, 96);
+
+        REQUIRE(calculated_gravity.x == Approx(expected_gravity.x));
+        REQUIRE(calculated_gravity.y == Approx(expected_gravity.y));
+    }
 }
