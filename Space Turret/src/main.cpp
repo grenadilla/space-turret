e06f854bce669556a8608776e6a91cf7caeecc03
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2019-12-10 17:41:59 -0600

    Make window unresizable

--- Space Turret/src/main.cpp
+++ Space Turret/src/main.cpp
@@ -3,11 +3,13 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    ofGLFWWindowSettings settings;
+    settings.setSize(1024, 768);  // <---- Set the window dimensions.
+    settings.resizable = false;
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+    shared_ptr<ofAppBaseWindow> window = ofCreateWindow(settings);
+    shared_ptr<ofApp> app(new ofApp());
 
+    ofRunApp(window, app);
+    ofRunMainLoop();
 }
